d4b7b0c9219bc47a14561ef6f533badd74863c2e
ashen007 <asheniranga@gmail.com>
2021-02-22 19:37:36 +0530

    GradientDecent class added

--- models/regression models.py
+++ models/regression models.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error
 
 
 class OLS:
@@ -15,11 +15,46 @@ class OLS:
 
     @staticmethod
     def prediction(x, theta, intercept):
+        """
+        :param x: predicts
+        :param theta: coefficients
+        :param intercept: intercept
+        :return: predicted y values
+        """
         predict_y = np.dot(x, theta) + intercept
         return predict_y
 
     @staticmethod
     def score(y_true, y_predict):
-        r2Score = r2_score(y_true,y_predict)
-        mse = mean_squared_error(y_true,y_predict)
-        return r2Score,mse
+        """
+        :param y_true: real target feature values
+        :param y_predict: model predicted target values
+        :return: square error and mean square error
+        """
+        r2Score = r2_score(y_true, y_predict)
+        mse = mean_squared_error(y_true, y_predict)
+        return r2Score, mse
+
+
+class GradientDecent:
+    @staticmethod
+    def cost_function(x, y, theta):
+        cost = np.sum((np.dot(x, theta) - y) ** 2)
+        return cost
+
+    @staticmethod
+    def fit(x, y, learning_rate, epochs):
+        theta = np.zeros(x.shape[1])
+        ones = np.ones((x.shape[0], 1))
+        x = np.concatenate((np.asarray(x), ones))
+        cost_ = []
+
+        for i in range(epochs):
+            hypothesis = np.dot(x, theta)
+            loss = hypothesis - y
+            gradient = 2 * np.dot(x.T, loss)
+            theta = theta - gradient * learning_rate
+            cost = GradientDecent.cost_function(x,y,theta)
+            cost_.append(cost)
+
+        return theta, cost_
