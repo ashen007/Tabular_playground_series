038e434f74c8cc8b3638bd180447a288578e0e11
ashen007 <asheniranga@gmail.com>
2021-02-23 14:20:45 +0530

    RidgeRegression class added

--- models/RegressionModels.py
+++ models/RegressionModels.py
@@ -82,3 +82,52 @@ class GradientDecent:
         r2Score = r2_score(y_true, y_predict)
         mse = mean_squared_error(y_true, y_predict)
         return r2Score, mse
+
+    class RidgeRegression:
+        def __init__(self, alpha):
+            self.alpha = alpha
+
+        def cost_function(self, x, y, theta):
+            cost = (np.sum((np.dot(x, theta) - y) ** 2) + self.alpha * np.sum(theta ** 2)) / 2 * len(y)
+            return cost
+
+        @staticmethod
+        def fit(x, y, learning_rate, epochs):
+            theta = np.zeros(x.shape[1] + 1)
+            ones = np.ones(x.shape[0])
+            x = np.hstack((np.asarray(x), ones.reshape(-1, 1)))
+            cost_ = []
+
+            for i in range(epochs):
+                hypothesis = np.dot(x, theta)
+                loss = hypothesis - y
+                gradient = np.dot(x.T, loss) / len(y)
+                theta = theta - gradient * learning_rate
+                cost = RidgeRegression.cost_function(x, y, theta)
+                cost_.append(cost)
+
+            return theta, cost_
+
+        @staticmethod
+        def prediction(x, theta):
+            """
+            :param x: predicts
+            :param theta: coefficients
+            :param intercept: intercept
+            :return: predicted y values
+            """
+            ones = np.ones((x.shape[0], 1))
+            x = np.hstack((np.asarray(x), ones.reshape(-1, 1)))
+            predict_y = np.dot(x, theta)
+            return predict_y
+
+        @staticmethod
+        def score(y_true, y_predict):
+            """
+            :param y_true: real target feature values
+            :param y_predict: model predicted target values
+            :return: square error and mean square error
+            """
+            r2Score = r2_score(y_true, y_predict)
+            mse = mean_squared_error(y_true, y_predict)
+            return r2Score, mse
